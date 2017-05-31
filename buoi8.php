ecb6193ce7336da5ffa3f78f86b8b724eac93d70
Huong Huong <Huong Huong>
2017-05-31 14:27:17 +0700

    buoi8

---
+++ buoi8.php
@@ -0,0 +1,82 @@
+<?php
+
+// $a = '12'; //12
+// var_dump($a);
+
+
+// if($a === 12){
+// 	echo $a+1; //13
+// }
+// else{
+// 	echo "Đây không phải số";
+// }
+/*
+$bool = false;
+if($bool){
+	echo "có bool";
+}
+else{
+	echo "không có bool";
+}
+var_dump($bool);
+*/
+
+// $a = (int)12.5;
+// var_dump($a);
+
+
+$arr = array("key"=>'value','key2'=>"value2"); // mảng có khóa do người dùng tự định nghĩa
+/*
+	Array
+	(
+	    [key] => value
+	    [key2] => value2
+	)
+
+*/
+//echo $arr;
+//print_r($arr);
+
+//echo $arr['key2'];
+
+// $arr2 = array('value',"value2"); // mảng có khóa tự động
+// // print_r($arr2);
+
+// // echo $a = $arr2[1];
+
+
+// // echo json_encode($arr);
+
+// $json = '{"key":"value","key2":"value2"}'; //kiểu json
+
+
+// class lop{
+// 	///$b = 'chào bạn';
+// 	function php(){
+// 		return "Chào lớp php";
+// 	}
+// }
+
+
+
+// $class = new lop();
+// echo $class->php();
+// //var_dump($class);
+
+
+
+// $a;
+// echo "chào $a";
+//Hằng
+$pi=3.14;
+define('pi',3.14);
+//define('pi',3.1);
+echo pi;
+
+
+
+
+
+
+
+?>
